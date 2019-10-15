9d77dee831961422b85097e0989377e8d18fb147
jinalbangur16 <47166007+jinalbangur16@users.noreply.github.com>
2019-10-15 18:24:11 -0400

    Update test13.py

--- us_test/sprint02_tests/test13.py
+++ us_test/sprint02_tests/test13.py
@@ -1,7 +1,6 @@
 from userstories.sprint02_us.userStory13 import us13_siblings_spacing
 import unittest 
 
-
 class UserStory13Test(unittest.TestCase):
     def test_us13(self):
         
@@ -10,6 +9,7 @@ class UserStory13Test(unittest.TestCase):
        self.assertEqual(error_list,['ERROR: FAMILY: US13: The spacing between sibling1:I19 and sibling2:I26 of family:F23 is not valid',
                                     'ERROR: FAMILY: US13: The spacing between sibling1:I27 and sibling2:I19 of family:F23 is not valid',
                                     'ERROR: FAMILY: US13: The spacing between sibling1:I27 and sibling2:I26 of family:F23 is not valid'])        
-if __name__ == '__main__':
+
+    if __name__ == '__main__':
     # note: there is no main(). Only test cases here
     unittest.main(exit=False, verbosity=2)     
