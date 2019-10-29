054534cdbfa68ff94235a6ccb88d7822242c4ebe
jinalbangur <44354264+jinalbangur@users.noreply.github.com>
2019-10-29 17:45:49 -0400

    Create test17.py

---
+++ us_test/sprint03_tests/test17.py
@@ -0,0 +1,13 @@
+from userstories.sprint03_us.userStory17 import us17_no_marr_2_children
+import unittest 
+
+class UserStory17Test(unittest.TestCase):
+
+    def test_us17(self):
+       error_list = us17_no_marr_2_children("gedfilestest/sprint03_ged/us117testdata.ged")
+       print(error_list)
+       self.assertEqual(error_list,['ERROR: FAMILY: US17: F23 Parents should not marry their descendants or parents'])
+    
+if __name__ == '__main__':
+    # note: there is no main(). Only test cases here
+    unittest.main(exit=False, verbosity=2)     
