2a63e7cd9e4524fc087076ad2758e8c7a9a7218d
ZainRaza14 <zainraza500@yahoo.com>
2019-10-29 02:28:24 -0400

    Update test24.py

--- us_test/sprint03_tests/test24.py
+++ us_test/sprint03_tests/test24.py
@@ -8,11 +8,11 @@ class UserStory24Test(unittest.TestCase):
        error_list = us24_unique_fam_by_spouses("gedfilestest/sprint03_ged/us24testdata.ged")
        print(error_list)
        self.assertEqual(error_list,["ERROR: US24 : F23 husband, wife names or marriage date is occurring twice in Gedcom ", 
-       	"ERROR: US24 : F23 husband, wife names or marriage date is occurring twice in Gedcom ", 
-       	"ERROR: US24 : F24 husband, wife names or marriage date is occurring twice in Gedcom ",
-       	"ERROR: US24 : F24 husband, wife names or marriage date is occurring twice in Gedcom ",
-       	"ERROR: US24 : F24 husband, wife names or marriage date is occurring twice in Gedcom ",
-       	"ERROR: US24 : F24 husband, wife names or marriage date is occurring twice in Gedcom "])
+                                    "ERROR: US24 : F23 husband, wife names or marriage date is occurring twice in Gedcom ", 
+                                    "ERROR: US24 : F24 husband, wife names or marriage date is occurring twice in Gedcom ",
+                                    "ERROR: US24 : F24 husband, wife names or marriage date is occurring twice in Gedcom ",
+                                    "ERROR: US24 : F24 husband, wife names or marriage date is occurring twice in Gedcom ",
+                                    "ERROR: US24 : F24 husband, wife names or marriage date is occurring twice in Gedcom "])
      
 if __name__ == '__main__':
     # note: there is no main(). Only test cases here
