884d798561c2a1151edcdf6304dc7e6a7701809a
neilgupte75 <55360105+neilgupte75@users.noreply.github.com>
2019-11-04 19:12:53 -0500

    updated us22

--- userstories/sprint03_us/userStory22.py
+++ userstories/sprint03_us/userStory22.py
@@ -7,13 +7,20 @@ Created on Thu Oct 24 21:24:33 2019
 
 from ParserModule import parse_main
 from print_main import printTables
+import re
+def atoi(text):
+    return int(text) if text.isdigit() else text
 
-
+def natural_keys(text):
+    return [ atoi(c) for c in re.split('(\d+)',text) ]
+    
 def us22_unique_IDs(file):
     #printTables(file)
     indDict,famDict,errorList = parse_main(file)
     error_list=list()
     errorList=list(errorList)
+    errorList.sort(key=natural_keys)
+
     if errorList:
         error_string=f"ERROR: US22 : The ids:{errorList} are repeated"
         error_list.append(error_string)
