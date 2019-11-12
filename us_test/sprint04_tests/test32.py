92eaace575fda6117d0b49fba672bac6ee9f892a
jinalbangur16 <47166007+jinalbangur16@users.noreply.github.com>
2019-11-12 18:44:37 -0500

    Update test32.py

--- us_test/sprint04_tests/test32.py
+++ us_test/sprint04_tests/test32.py
@@ -8,7 +8,6 @@ class UserStory32Test(unittest.TestCase):
        print(error_list)
        self.assertEqual(error_list,['ERROR: INDIVIDUAL: US32: The individual I01 has multiple births','ERROR: INDIVIDUAL: US32: The individual I02 has multiple births'])
 
-     
 if __name__ == '__main__':
     # note: there is no main(). Only test cases here
     unittest.main(exit=False, verbosity=2)     
